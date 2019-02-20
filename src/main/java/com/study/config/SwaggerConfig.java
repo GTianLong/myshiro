869501e9b0161d0e0be171a73709f89a4e6c42ef
gaotianlong <869553051@qq.com>
2019-02-20 10:12:04 +0800

    修改swagger扫描包的路径

--- src/main/java/com/study/config/SwaggerConfig.java
+++ src/main/java/com/study/config/SwaggerConfig.java
@@ -24,7 +24,7 @@ public class SwaggerConfig {
     public Docket docket(){
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
 //                   当前包路径
-                .apis(RequestHandlerSelectors.basePackage("com.reset.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.study.controller"))
                 .paths(PathSelectors.any()).build();
 
     }
