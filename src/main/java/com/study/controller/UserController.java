080d87332a4544dc34d1264e758702422d1e2706
gaotianlong <869553051@qq.com>
2019-02-21 09:55:43 +0800

    修改接口

--- src/main/java/com/study/controller/UserController.java
+++ src/main/java/com/study/controller/UserController.java
@@ -6,11 +6,9 @@ import com.study.model.UserRole;
 import com.study.service.UserRoleService;
 import com.study.service.UserService;
 import com.study.util.PasswordHelper;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,30 +21,19 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/users")
-@Api("HomeController")
 public class UserController {
-
     @Resource
     private UserService userService;
     @Resource
     private UserRoleService userRoleService;
 
-    @ApiOperation(value = "登录",notes = "登录")
-    /*@ApiImplicitParam(name ="id",value = "学生id",paramType = "path",required = true,dataType = "String")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id",value = "用户id",dataType = "String",paramType = "query",example = "1112")
-    })
-    @ApiResponses({
-            @ApiResponse(code=400,message = "请求参数没有填好"),
-            @ApiResponse(code=404,message="请求路径没有找到")
-    })*/
-    @RequestMapping(value = "/getAll",method = RequestMethod.POST)
+    @RequestMapping
     public Map<String,Object> getAll(User user, String draw,
                                      @RequestParam(required = false, defaultValue = "1") int start,
                                      @RequestParam(required = false, defaultValue = "10") int length){
         Map<String,Object> map = new HashMap<>();
         PageInfo<User> pageInfo = userService.selectByPage(user, start, length);
-//        System.out.println("pageInfo.getTotal():"+pageInfo.getTotal());
+        System.out.println("pageInfo.getTotal():"+pageInfo.getTotal());
         map.put("draw",draw);
         map.put("recordsTotal",pageInfo.getTotal());
         map.put("recordsFiltered",pageInfo.getTotal());
@@ -61,7 +48,7 @@ public class UserController {
      *  	  此处获取的参数的角色id是以 “,” 分隔的字符串
      * @return
      */
-    @RequestMapping(value = "/saveUserRoles",method = RequestMethod.POST)
+    @RequestMapping("/saveUserRoles")
     public String saveUserRoles(UserRole userRole){
         if(StringUtils.isEmpty(userRole.getUserid()))
             return "error";
@@ -74,7 +61,7 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    @RequestMapping(value = "/add")
     public String add(User user) {
         User u = userService.selectByUsername(user.getUsername());
         if(u != null)
@@ -91,15 +78,15 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value = "/delete",method = RequestMethod.POST)
+    @RequestMapping(value = "/delete")
     public String delete(Integer id){
-      try{
-          userService.delUser(id);
-          return "success";
-      }catch (Exception e){
-          e.printStackTrace();
-          return "fail";
-      }
+        try{
+            userService.delUser(id);
+            return "success";
+        }catch (Exception e){
+            e.printStackTrace();
+            return "fail";
+        }
     }
 
 }
