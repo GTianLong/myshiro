b4578c54ef8b418c3f423c312f6daf5d43676855
gaotianlong <869553051@qq.com>
2019-02-21 09:02:03 +0800

    修改swagger注解

--- src/main/java/com/study/controller/UserController.java
+++ src/main/java/com/study/controller/UserController.java
@@ -6,9 +6,11 @@ import com.study.model.UserRole;
 import com.study.service.UserRoleService;
 import com.study.service.UserService;
 import com.study.util.PasswordHelper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/users")
+@Api("HomeController")
 public class UserController {
 
     @Resource
@@ -28,7 +31,16 @@ public class UserController {
     @Resource
     private UserRoleService userRoleService;
 
-    @RequestMapping
+    @ApiOperation(value = "登录",notes = "登录")
+    /*@ApiImplicitParam(name ="id",value = "学生id",paramType = "path",required = true,dataType = "String")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id",value = "用户id",dataType = "String",paramType = "query",example = "1112")
+    })
+    @ApiResponses({
+            @ApiResponse(code=400,message = "请求参数没有填好"),
+            @ApiResponse(code=404,message="请求路径没有找到")
+    })*/
+    @RequestMapping(value = "/getAll",method = RequestMethod.POST)
     public Map<String,Object> getAll(User user, String draw,
                                      @RequestParam(required = false, defaultValue = "1") int start,
                                      @RequestParam(required = false, defaultValue = "10") int length){
@@ -49,7 +61,7 @@ public class UserController {
      *  	  此处获取的参数的角色id是以 “,” 分隔的字符串
      * @return
      */
-    @RequestMapping("/saveUserRoles")
+    @RequestMapping(value = "/saveUserRoles",method = RequestMethod.POST)
     public String saveUserRoles(UserRole userRole){
         if(StringUtils.isEmpty(userRole.getUserid()))
             return "error";
@@ -62,7 +74,7 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value = "/add")
+    @RequestMapping(value = "/add",method = RequestMethod.POST)
     public String add(User user) {
         User u = userService.selectByUsername(user.getUsername());
         if(u != null)
@@ -79,7 +91,7 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value = "/delete")
+    @RequestMapping(value = "/delete",method = RequestMethod.POST)
     public String delete(Integer id){
       try{
           userService.delUser(id);
